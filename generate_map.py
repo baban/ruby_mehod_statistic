8bf05af5b7db481a74ec1fef579fd77c95ebc8be
masanao.matsubara <m_matsubara@okwave.co.jp>
2017-06-11 01:24:58 +0900

    全体のメソッドの呼ばれ具合を量体数グラフの上にプロット

--- generate_map.py
+++ generate_map.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-f = open("sort_result.yml")
+f = open("aggrigate_result.yml")
 data = yaml.load(f)
 f.close()
 
@@ -20,26 +20,37 @@ print(data)
 
 matplotlib.pyplot.plot(data)
 
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.set_title('Discrete distribution map')
-ax.set_xlabel('rank')
-ax.set_ylabel('number of calls')
-
-ax.bar(list(range(1,len(data)+1)), data)
-fig.show()
-matplotlib.pyplot.savefig('distrubution_map.png')
-
 # メソッドの使用回数の順番でグラフにプロットを行う。
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(1,1,1)
-ax2.set_title('Discrete distribution map')
-ax2.set_xlabel('log(rank)')
-ax2.set_ylabel('log(number of calls)')
 
-# 両対数グラフでZipfの法則に従っているかを確認する
-left = np.array(list(map(lambda v: m.log(v), list(range(1,len(data)+1)))))
-right = np.array(list(map(lambda v: m.log(v), data)))
-ax2.plot(left, right)
-fig2.show()
-matplotlib.pyplot.savefig('log_distrubution_map.png')
+# 棒グラフで描画
+def plot_graph():
+    print("graph")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title('Discrete distribution map')
+    ax.set_xlabel('rank')
+    ax.set_ylabel('number of calls')
+
+    ax.bar(list(range(1,len(data)+1)), data)
+    fig.show()
+    matplotlib.pyplot.savefig('distrubution_map.png')
+
+plot_graph()
+
+# 両対数グラフでプロットする
+def plot_log_log_graph():
+    print("graph2")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title('Discrete distribution map')
+    ax.set_xlabel('log(rank)')
+    ax.set_ylabel('log(number of calls)')
+
+    # 両対数グラフでZipfの法則に従っているかを確認する
+    left = np.array(list(map(lambda v: m.log(v), list(range(1,len(data)+1)))))
+    right = np.array(list(map(lambda v: m.log(v), data)))
+    ax.plot(left, right)
+    fig.show()
+    matplotlib.pyplot.savefig('log_distrubution_map.png')
+
+plot_log_log_graph()
