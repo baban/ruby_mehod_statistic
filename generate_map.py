e1a35fcf3081bed66c9c6e70f65292f328f030ae
masanao.matsubara <m_matsubara@okwave.co.jp>
2017-06-11 10:59:55 +0900

    棒グラフの表示を上位100位に絞り込んで表示する

--- generate_map.py
+++ generate_map.py
@@ -5,6 +5,35 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+# 棒グラフで描画
+def plot_bar_graph(data):
+    print("bar graph")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title('Discrete distribution map')
+    ax.set_xlabel('rank')
+    ax.set_ylabel('number of calls')
+
+    ax.bar(list(range(1,len(data)+1)), data)
+    fig.show()
+    matplotlib.pyplot.savefig('distrubution_map.png')
+
+# 両対数グラフでプロットする
+def plot_log_log_graph(data):
+    print("log log graph")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title('Discrete distribution map')
+    ax.set_xlabel('log(rank)')
+    ax.set_ylabel('log(number of calls)')
+
+    # 両対数グラフでZipfの法則に従っているかを確認する
+    left = np.array(list(map(lambda v: m.log(v), list(range(1,len(data)+1)))))
+    right = np.array(list(map(lambda v: m.log(v), data)))
+    ax.plot(left, right)
+    fig.show()
+    matplotlib.pyplot.savefig('log_log_distrubution_map.png')
+
 f = open("aggrigate_result.yml")
 data = yaml.load(f)
 f.close()
@@ -18,39 +47,6 @@ data.reverse()
 
 print(data)
 
-matplotlib.pyplot.plot(data)
-
 # メソッドの使用回数の順番でグラフにプロットを行う。
-
-# 棒グラフで描画
-def plot_graph():
-    print("graph")
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.set_title('Discrete distribution map')
-    ax.set_xlabel('rank')
-    ax.set_ylabel('number of calls')
-
-    ax.bar(list(range(1,len(data)+1)), data)
-    fig.show()
-    matplotlib.pyplot.savefig('distrubution_map.png')
-
-plot_graph()
-
-# 両対数グラフでプロットする
-def plot_log_log_graph():
-    print("graph2")
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.set_title('Discrete distribution map')
-    ax.set_xlabel('log(rank)')
-    ax.set_ylabel('log(number of calls)')
-
-    # 両対数グラフでZipfの法則に従っているかを確認する
-    left = np.array(list(map(lambda v: m.log(v), list(range(1,len(data)+1)))))
-    right = np.array(list(map(lambda v: m.log(v), data)))
-    ax.plot(left, right)
-    fig.show()
-    matplotlib.pyplot.savefig('log_distrubution_map.png')
-
-plot_log_log_graph()
+plot_bar_graph(data[0:100])
+plot_log_log_graph(data)
